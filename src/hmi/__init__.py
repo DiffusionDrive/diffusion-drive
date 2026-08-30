be9944f4bf2ba6b725f0d83787537db232c8f871
captol12 <capitol1221@gmail.com>
2026-08-31 00:56:28 +0800

    Installed Kivy framework in hmi folder     Code present to test successful installation

---
+++ src/hmi/__init__.py
@@ -0,0 +1,15 @@
+#https://www.youtube.com/watch?v=POwi9Don3pE
+#test for successful Kivy installation
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        return Button(text="Click Me!", on_press=self.on_button_click)
+
+    def on_button_click(self, instance):
+        instance.text = "Clicked!"
+
+if __name__ == "__main__":
+    MyApp().run()
