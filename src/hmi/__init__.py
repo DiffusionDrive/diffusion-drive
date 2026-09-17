5e8a5008cfc1249afcd0f8c0b8b1769ae87350d4
captol12 <capitol1221@gmail.com>
2026-09-17 17:24:45 +0800

    hmi: implemented kivy-garden/mapview into hmi/__init__.py

--- src/hmi/__init__.py
+++ src/hmi/__init__.py
@@ -1,13 +1,17 @@
 # https://kivymd.readthedocs.io/en/latest/getting-started/
 # test for successful KivyMD installation
 
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+# https://github.com/kivy-garden/mapview
+# test for successful MapView integration
 
+from kivymd.app import MDApp
+# from kivymd.uix.label import MDLabel
+from kivy_garden.mapview import MapView
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello, World", halign="center")
+        mapview = MapView(zoom=11, lat=50.6394, lon=3.057)
+        return mapview
 
 
 MainApp().run()
